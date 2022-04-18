9b371c0b75a3c433dd753f70bb7984fa0295ade1
irisgarcialeon <irisgarcialeon@gmail.com>
2022-04-18 08:37:33 +0200

    Add unit conversion

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// Date and time //
 function formatDate (timestamp){
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -22,7 +22,7 @@ function formatDate (timestamp){
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`
 }
-
+// Temperature display //
 function displayTemperature (response) {
   let temperatureElement = document.querySelector("#current-degrees");
   let cityElement = document.querySelector("#city");
@@ -32,7 +32,9 @@ function displayTemperature (response) {
   let dateElement = document.querySelector("#current-time");
   let iconElement = document.querySelector("#icon");
 
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -40,8 +42,45 @@ function displayTemperature (response) {
   dateElement.innerHTML = formatDate(response.data.dt*1000);
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
-let apiKey = "7d88c15d68c158185437db8adc9adf90";
-let city ="Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+// Search Engine //
+function search(city){
+let apiKey = "7d88c15d68c158185437db8adc9adf90";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+// Unit conversion //
+
+function displayFahrenheitTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperatures");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelsiusLinkTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperatures");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusLinkTemperature);
+
